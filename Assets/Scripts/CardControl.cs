bd4096255e772a4b25a77669955f9af20dbb6636
Cloppi <mycloppi@gmail.com>
2017-01-21 04:40:31 +0300

    Copyed all project files
    Copyed all project files

---
+++ Assets/Scripts/CardControl.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CardControl : MonoBehaviour {
+
+	public Animator anim;
+	public bool open;
+	public GameObject sound;
+
+	// Use this for initialization
+	public void OpenCard () {
+		open = !open;
+		anim.SetBool ("Open", open);
+		Instantiate (sound);
+	}
+
+}
---
+++ Assets/Scripts/SoundDestroyer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundDestroyer : MonoBehaviour {
+
+	public float time;
+	private float timer;
+	
+	// Update is called once per frame
+	void Update () {
+		timer += Time.deltaTime;
+		if (timer > time) 
+		{
+			Destroy (gameObject);
+		}
+	}
+}
